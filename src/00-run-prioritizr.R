e27885221800795074a18f86f988b6301a2f3049
Brandon Edwards <b_edwards@ducks.ca>
2025-05-14 14:57:41 -0500

    run prioritizr with various budgets

--- src/00-run-prioritizr.R
+++ src/00-run-prioritizr.R
@@ -26,18 +26,32 @@ tl_rast <- rasterize(tl,
                      rast(ext(divers), resolution=res(divers), crs = crs(divers))) |>
   subst(from = 1, to = 0, others = 1)
 
-pu <- rast(divers, vals = 0.01)
+pu <- rast(divers, vals = 1/1000)
 divers <- divers / 1000
 dabblers <- dabblers / 1000
 redh <- redh / 1000
 canv <- canv / 1000
 
-p1 <- problem(pu, c(redh, canv)) %>%
-  add_locked_out_constraints(tl_rast) %>%
-  add_relative_targets(c(0.55,0.59)) %>%
-  add_min_shortfall_objective(budget = 1000) %>%
-  add_gurobi_solver()
+top_budget <- freq(tl_rast, value = 0)$count / 1000
 
-s1 <- solve(p1, force = TRUE)
+budgets <- seq(9.7915, top_budget, length.out = 100)
+
+portfolio <- vector(mode = "list", length = length(budgets))
+
+for (i in 1:length(budgets))
+{
+  print(i)
+  prob <- problem(pu, c(redh, canv)) %>%
+    add_locked_out_constraints(tl_rast) %>%
+    add_relative_targets(c(0.55,0.59)) %>%
+    add_min_shortfall_objective(budget = budgets[i]) %>%
+    add_gurobi_solver()
+  
+  portfolio[[i]] <- solve(prob, force = TRUE)
+}
+
+portfolio_rast <- rast(portfolio)
+
+priority <- sum(portfolio_rast)
 
 ####### Output ####################################
